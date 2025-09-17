62256097785729e9b43fd50025088a957fdac4d1
lnkhrnk <alinasun190@gmail.com>
2025-09-17 20:18:55 +0300

    Добавлены модели Car, Client, Order

---
+++ src/Client.java
@@ -0,0 +1,44 @@
+public class Client {
+    private String name;
+    private String email;
+    private double budget;
+
+    public Client(String name, String email, double budget) {
+        this.name = name;
+        this.email = email;
+        this.budget = budget;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", budget=" + budget +
+                '}';
+    }
+}
